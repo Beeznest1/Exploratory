3328272868b48a078677ad7417e192bc93bf3230
ahmadrashed7 <rashed.mahazi@gmail.com>
2023-02-17 11:32:04 -0800

    locators contains set of indentifiers and selectors that are used to locate element on the Beeznests web page

---
+++ Locators.py
@@ -0,0 +1,7 @@
+# Registration requirements
+Register_Tab = "//a[normalize-space()='REGISTER']"
+Email_Address = "email"
+Password = "password"
+Confirm_Password = "confirm-password"
+Checkbox = "//input[@type='checkbox']"
+Register = "//span[@class='tw-font-bold tw-ml-1']"
