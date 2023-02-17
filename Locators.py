8ec120a4c0c18fd2c3554174bed5b78879e0f4ad
ahmadrashed7 <rashed.mahazi@gmail.com>
2023-02-17 11:43:45 -0800

    logout selectors has been added

--- Locators.py
+++ Locators.py
@@ -5,3 +5,8 @@ Password = "password"
 Confirm_Password = "confirm-password"
 Checkbox = "//input[@type='checkbox']"
 Register = "//span[@class='tw-font-bold tw-ml-1']"
+
+# logout
+DropDown_Menu = "/html/body/div/header/div[1]/div/nav[2]/div/span/img"
+Logout_button = "/html/body/div/header/div[1]/div/nav[2]/div/div/div/span"
+
