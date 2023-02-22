b5dde5d9b739bf81ce5ae6506452d0379bfb0182
ahmadrashed7 <120500979+ahmadrashed7@users.noreply.github.com>
2023-02-22 12:03:34 -0800

    Update Locators.py

--- Locators.py
+++ Locators.py
@@ -10,3 +10,10 @@ Register = "//span[@class='tw-font-bold tw-ml-1']"
 DropDown_Menu = "/html/body/div/header/div[1]/div/nav[2]/div/span/img"
 Logout_button = "/html/body/div/header/div[1]/div/nav[2]/div/div/div/span"
 
+# requirement for posting a project
+PROJECTS = "/html/body/div/header/div[1]/div/nav[1]/a[1]/span"
+Post_A_New_Project = '/html/body/div/div[1]/div[2]/a'
+position = "(//input[@type='text'])[1]"
+ProjectName = "(//input[@type='text'])[2]"
+category = "//div[@class='v-select__selections']"
+Cloud_Computing_Engineers = "//div[contains(text(),'Cloud Computing Engineers')]"
